5268f45805e45fba28c68f2ae38c370645b1ccd8
Chris Baucom <crbaucom@gmail.com>
2018-02-28 17:47:46 -0500

    Add search component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
 import Login from './Login'
+import Search from './Search'
 import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css'
 
@@ -13,9 +14,10 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route exact path="/login" component={Login} />
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/search" component={Search} />
           </Switch>
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,13 +11,17 @@ class Header extends Component {
         <div className="flex flex-fixed black">
           <div className="fw7 mr1">Hacker News</div>
           <Link to="/" className="ml1 no-underline black">
-            new
+            News
+          </Link>
+          <div className="ml1">|</div>
+          <Link to="/search" className="ml1 no-underline black">
+            search
           </Link>
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
               <Link to="/create" className="ml1 no-underline black">
-                submit
+                Submit
               </Link>
             </div>
           )}
